6ff190e3df237ec48411f4cdad3717fdb1b54c24
uoaper <vovapertsov@gmail.com>
2018-01-29 19:00:12 +0300

    Going to change architecture to Core Data model

---
+++ Nupsa/CoreDataStack.swift
@@ -0,0 +1,52 @@
+//
+//  CoreDataStack.swift
+//  TodoList
+//
+//  Created by Vladimir Pertsov on 24.01.18.
+//  Copyright © 2018 Vladimir Pertsov. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    
+    lazy var managedObjectContext: NSManagedObjectContext = {
+        let container = self.persistentContainer
+        
+        return container.viewContext
+    } ()
+    
+    private lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "TodoList")
+        container.loadPersistentStores() { storeDescription, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error: \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+}
+
+
+extension NSManagedObjectContext {
+    func saveChanges() {
+        if self.hasChanges {
+            do {
+                try save()
+            } catch {
+                fatalError("Error: \(error.localizedDescription)")
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
--- Nupsa/SortingVIew.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SortingVIew.swift
-//  Nupsa
-//
-//  Created by Vladimir Pertsov on 12.01.18.
-//  Copyright © 2018 Vladimir Pertsov. All rights reserved.
-//
-
-import Foundation
